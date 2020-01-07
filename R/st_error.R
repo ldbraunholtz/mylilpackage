13ad038e353c214bb3ff8342c6f635fcdbed45ed
lbraunholtz <l.d.braunholtz2@ncl.ac.uk>
2020-01-07 13:30:19 +0000

    adding standard error function

---
+++ R/st_error.R
@@ -0,0 +1,13 @@
+#' Calculate standard error of Large sample
+#'
+#' @param vect numerical vector
+#' @importFrom stats sd
+#'
+#' @return standard error
+#' @export
+#'
+#' @examples
+#' st_error(rnorm(50))
+st_error <- function(vect){
+  sd(vect)/sqrt(sum(vect))
+}
