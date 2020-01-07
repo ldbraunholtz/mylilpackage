c8db58fb0f871b17612f11a34dd69d2533c831c8
lbraunholtz <l.d.braunholtz2@ncl.ac.uk>
2020-01-07 12:21:56 +0000

    adding the description file using roxygen

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,3 +1,14 @@
+#' Calculate sum squares for a sample
+#'
+#'Calculates the sum of squared deviations from mean, SS(x) of a numeric vector
+#'
+#' @param vect numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares(rnorm(50))
 sum_squares <- function(vect){
   sum((vect - mean(vect))^2)
 }
